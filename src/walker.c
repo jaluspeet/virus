f16701972904d5e062635ecfc1a7d293797431c8
jaluspeet <jaluspit@gmail.com>
2023-12-23 08:27:12 +0100

    fixed noise caused by walker

--- src/walker.c
+++ src/walker.c
@@ -15,39 +15,45 @@ void walker(int map[MAP_X][MAP_Y], int flag) {
   for (int i = 0; i < WALKER_SIZE && tries < WALKER_TRIES; i++) {
     switch (direction) {
     case 0:
-      if (position.x < MAP_X - 1 && map[position.x + 1][position.y] == 0)
+      if (position.x < MAP_X - 1 && map[position.x + 1][position.y] == 0) {
         position.x++;
+        map[position.x][position.y] = flag;
+      }
       else {
         direction = rand() % 4;
         tries++;
       }
       break;
     case 1:
-      if (position.x > 0 && map[position.x - 1][position.y] == 0)
+      if (position.x > 0 && map[position.x - 1][position.y] == 0) {
         position.x--;
+        map[position.x][position.y] = flag;
+      }
       else {
         direction = rand() % 4;
         tries++;
       }
       break;
     case 2:
-      if (position.y < MAP_Y - 1 && map[position.x][position.y + 1] == 0)
+      if (position.y < MAP_Y - 1 && map[position.x][position.y + 1] == 0) {
         position.y++;
+        map[position.x][position.y] = flag;
+      }
       else {
         direction = rand() % 4;
         tries++;
       }
       break;
     case 3:
-      if (position.y > 0 && map[position.x][position.y - 1] == 0)
+      if (position.y > 0 && map[position.x][position.y - 1] == 0) {
         position.y--;
+        map[position.x][position.y] = flag;
+      }
       else {
         direction = rand() % 4;
         tries++;
       }
       break;
     }
-
-    map[position.x][position.y] = flag;
   }
 }
